186d63754acd3dc9039a3cd0d7da892caa4028b8
lincolnneres <lincolnneres@gmail.com>
2024-08-10 16:20:55 -0300

    adicionado mais um codigo

--- scripts.js
+++ scripts.js
@@ -2,5 +2,11 @@ function generateNumber() {
   let min = Math.ceil(document.querySelector(".input-min").value);
   let max = Math.floor(document.querySelector(".input-max").value);
 
-  let result = Math.floor(Math.random() * (max - min + 1) + min);
+  if (max > min) {
+    let result = Math.floor(Math.random() * (max - min + 1) + min);
+
+    alert(result);
+  } else {
+    alert("o valor minimo tem que MENOR que o valor máximo.");
+  }
 }
